882aa061dc634cf31d483700de77eebcc5b90a34
Daniel Kunz <dkunz@intera.de>
2020-02-17 13:34:38 +0100

    [TASK] Make adjustments for integrating into typo3 v9

--- Classes/XmlSitemap/PagesXmlSitemapDataProvider.php
+++ Classes/XmlSitemap/PagesXmlSitemapDataProvider.php
@@ -21,7 +21,7 @@ use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Context\LanguageAspect;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryHelper;
-use TYPO3\CMS\Core\Domain\Repository\PageRepository;
+use TYPO3\CMS\Frontend\Page\PageRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '10.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.1.0'
+            'typo3' => '9.5.13'
         ],
         'conflicts' => [],
         'suggests' => [],
